e530538b58af0d348efe8a6c3bbedca829c9dad4
Drew <tullos.andrew@gmail.com>
2024-09-20 16:34:18 -0500

    Refactored code

--- qr.py
+++ qr.py
@@ -1,4 +1,12 @@
 import qrcode
+
+
+data = input("Enter text or URL: ").strip
+filename = ("Enter your filename: ").strip
+qr = qrcode.QRCode(box_size=10, border=4)
+
+qr.add_data(data)
+qr.make_image(fill_color="black", back_color="blue")
 img = qrcode.make('https://main.d3vianaxlv4x9d.amplifyapp.com/')
 type(img) 
 img.save("some_file.png")
